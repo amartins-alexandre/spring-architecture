acba84d7f43d57e0a9492d97224efa9b01c1a01c
amartins-alexandre <8130295+amartins-alexandre@users.noreply.github.com>
2024-02-25 01:35:25 -0300

    feature(config-client): client-side externalized configuration

---
+++ ms-one/src/test/kotlin/br/com/amartins/springarchtecture/msone/MsOneApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.amartins.springarchtecture.msone
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsOneApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
